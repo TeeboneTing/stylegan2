523604a3ca15061b9b6484b72f02634c81bb33ab
Teebone Ding <teebone.ding@ubitus.net>
2020-02-26 15:35:12 +0800

    add breakpoint for debugging

--- run_training.py
+++ run_training.py
@@ -79,7 +79,7 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
         G.fmap_base = D.fmap_base = 8 << 10
 
     # Transfer learning for config-f
-    if config_id is 'config-f-transfer':
+    if config_id == 'config-f-transfer':
         G.transfer_learning = D.transfer_learning = True
 
     # Config E: Set gamma to 100 and override G & D architecture.
@@ -119,6 +119,8 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
 
     if gamma is not None:
         D_loss.gamma = gamma
+
+    import pdb; pdb.set_trace()
 
     sc.submit_target = dnnlib.SubmitTarget.LOCAL
     sc.local.do_not_copy_source_files = True
